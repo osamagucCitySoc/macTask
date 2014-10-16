6526b50c4cc585f3944587052ca65e1b78181a85
osamagucCitySoc <osama.cs.eng@gmail.com>
2014-10-16 18:08:22 +0400

    1- added the template project

---
+++ task/MainViewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewTableViewController.h
+//  task
+//
+//  Created by OsamaMac on 10/16/14.
+//  Copyright (c) 2014 VF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewTableViewController : UITableViewController
+
+@end
